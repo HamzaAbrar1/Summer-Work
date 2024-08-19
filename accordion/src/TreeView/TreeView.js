d87f86b279a663f2aae9368d5fef9650a84324e1
Hamza Abrar <hamzaabrar11@gmail.com>
2024-08-20 01:05:14 +0500

    some changes made in the tree view folder

--- accordion/src/TreeView/TreeView.js
+++ accordion/src/TreeView/TreeView.js
@@ -1,31 +1,28 @@
 import React, { useState } from "react";
 
-export default function TreeView({ Data = [] }) {
-const [state,setState]=useState(false)
-const method1=()=>{
-setState(!state);
-}
-    return (
-    <div>
-      <h1 className=" inline mr-3">{Data.name}</h1>
-      {Data.children && Data.children.length>0? <div
-      onClick={()=>{
-        method1()
-      }} className="cursor-pointer inline" >+</div>:null }
-      <br />
-    {state ?  Data.children
-        ? Data.children.map((elem) => (
-            <div>
-              <div>{elem.name} </div>
+export default function TreeView({ Data = {} }) {
+  const [state, setState] = useState(false);
 
-              {elem.children && elem.children.length > 0 ? (
-                <div>
-                  <TreeView Data={elem} />
-                </div>
-              ) : null}
-            </div>
-          ))
-        : null:null }
+  const toggleState = () => {
+    setState(!state);
+  };
+
+  return (
+    <div>
+      <h1 className="inline mr-3">{Data.name}</h1>
+      {Data.children && Data.children.length > 0 && (
+        <div onClick={toggleState} className="cursor-pointer inline">
+          {state ? "-" : "+"}
+        </div>
+      )}
+      <br />
+      {state && Data.children && (
+        <div style={{ paddingLeft: "20px" }}>
+          {Data.children.map((elem) => (
+            <TreeView key={elem.name} Data={elem} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
